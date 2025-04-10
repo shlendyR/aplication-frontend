17e126cd6fc85ddd60552c0352de4740172a73d2
Shlendy Rincon <shlendyrincon13@gmail.com>
2025-04-10 11:33:44 -0400

    detalles

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ const Typography = React.lazy(() => import('./views/theme/typography/Typography'
 //my rutes camila
 const Users = React.lazy(() => import('./views/users/users.js'))
 const products = React.lazy(() => import('./views/products/products.js'))
+const setting = React.lazy(() => import('./views/setting/setting.js'))
 // Base
 const Accordion = React.lazy(() => import('./views/base/accordion/Accordion'))
 const Breadcrumbs = React.lazy(() => import('./views/base/breadcrumbs/Breadcrumbs'))
@@ -102,6 +103,7 @@ const routes = [
   //my path camila
   { path: '/users', name: 'Users', element: Users },
   { path: '/products', name: 'Products', element: products },
+  { path: '/setting', name: 'Setting', element: setting },
 ]
 
 export default routes
